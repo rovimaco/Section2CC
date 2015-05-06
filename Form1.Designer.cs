af032b6ad37e5d4b938667b342a657d23fa430c5
rovimaco <rvmcollins@gmail.com>
2015-05-06 15:01:30 +1200

    Section 2CC
    Checkpoint

---
+++ Form1.Designer.cs
@@ -0,0 +1,345 @@
+﻿namespace Section2CC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtAvgDepth = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtSurfaceArea = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.lblCategory = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.txtTableAvgTemp = new System.Windows.Forms.TextBox();
+            this.txtTableDollars = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(19, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pool Size and Cost";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "&Length";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "&Width";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Average &Depth";
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(140, 80);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(141, 20);
+            this.txtLength.TabIndex = 4;
+            this.txtLength.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(140, 122);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(141, 20);
+            this.txtWidth.TabIndex = 5;
+            this.txtWidth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtAvgDepth
+            // 
+            this.txtAvgDepth.Location = new System.Drawing.Point(140, 162);
+            this.txtAvgDepth.Name = "txtAvgDepth";
+            this.txtAvgDepth.Size = new System.Drawing.Size(141, 20);
+            this.txtAvgDepth.TabIndex = 6;
+            this.txtAvgDepth.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(288, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "m";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(288, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(15, 13);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "m";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(288, 170);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(15, 13);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "m";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(140, 213);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(140, 31);
+            this.btnCalculate.TabIndex = 10;
+            this.btnCalculate.Text = "&Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(25, 279);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Surface Area";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(28, 329);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(86, 13);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Volume of Water";
+            // 
+            // txtSurfaceArea
+            // 
+            this.txtSurfaceArea.Location = new System.Drawing.Point(140, 279);
+            this.txtSurfaceArea.Name = "txtSurfaceArea";
+            this.txtSurfaceArea.Size = new System.Drawing.Size(141, 20);
+            this.txtSurfaceArea.TabIndex = 13;
+            this.txtSurfaceArea.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(140, 321);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.Size = new System.Drawing.Size(141, 20);
+            this.txtVolume.TabIndex = 14;
+            this.txtVolume.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(291, 285);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(52, 13);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "sq metres";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(294, 328);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(62, 13);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "litres at 25C";
+            // 
+            // lblCategory
+            // 
+            this.lblCategory.AutoSize = true;
+            this.lblCategory.Location = new System.Drawing.Point(31, 376);
+            this.lblCategory.Name = "lblCategory";
+            this.lblCategory.Size = new System.Drawing.Size(0, 13);
+            this.lblCategory.TabIndex = 17;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(502, 11);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(155, 13);
+            this.label12.TabIndex = 18;
+            this.label12.Text = "Cost to heat pool to 25 degrees";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(503, 28);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(127, 13);
+            this.label13.TabIndex = 19;
+            this.label13.Text = "Celsius, based on varying";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(504, 44);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(134, 13);
+            this.label14.TabIndex = 20;
+            this.label14.Text = "average daily temperatures";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(470, 79);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(56, 13);
+            this.label15.TabIndex = 21;
+            this.label15.Text = "Avg Temp";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(610, 79);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(65, 13);
+            this.label16.TabIndex = 22;
+            this.label16.Text = "$ Per Month";
+            // 
+            // txtTableAvgTemp
+            // 
+            this.txtTableAvgTemp.Location = new System.Drawing.Point(433, 106);
+            this.txtTableAvgTemp.Multiline = true;
+            this.txtTableAvgTemp.Name = "txtTableAvgTemp";
+            this.txtTableAvgTemp.Size = new System.Drawing.Size(126, 344);
+            this.txtTableAvgTemp.TabIndex = 23;
+            this.txtTableAvgTemp.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtTableDollars
+            // 
+            this.txtTableDollars.Location = new System.Drawing.Point(591, 106);
+            this.txtTableDollars.Multiline = true;
+            this.txtTableDollars.Name = "txtTableDollars";
+            this.txtTableDollars.Size = new System.Drawing.Size(121, 344);
+            this.txtTableDollars.TabIndex = 24;
+            this.txtTableDollars.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 490);
+            this.Controls.Add(this.txtTableDollars);
+            this.Controls.Add(this.txtTableAvgTemp);
+            this.Controls.Add(this.label16);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.lblCategory);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtSurfaceArea);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtAvgDepth);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Pool Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtAvgDepth;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtSurfaceArea;
+        private System.Windows.Forms.TextBox txtVolume;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label lblCategory;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.TextBox txtTableAvgTemp;
+        private System.Windows.Forms.TextBox txtTableDollars;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Section2CC
+
+	// Rose Collins Student ID 90048876
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		// input validation on length, width, avg depth values as three sep funcs
+
+		const float txtLengthUpper = 50;
+		const float txtLengthLower = 5;
+		const float txtWidthUpper = 20;
+		const float txtWidthLower = 2;
+		const float txtAvgDepthUpper = 4;
+		const float txtAvgDepthLower = 2;
+
+
+
+		private void btnCalculate_Click( object sender, EventArgs e )
+		{
+			if( !ValidLength( float.Parse( txtLength.Text ) ) )
+			{
+				return;
+			}
+			if( !ValidWidth( float.Parse( txtWidth.Text ) ) )
+			{
+				return;
+			}
+			if( !ValidAvgDepth( float.Parse( txtAvgDepth.Text ) ) )
+			{
+				return;
+			}
+
+			var PoolLength = float.Parse( txtLength.Text );
+			var PoolWidth = float.Parse( txtWidth.Text );
+			var TotalSurfaceArea = CalculateSurfaceArea( PoolLength, PoolWidth );
+			var PoolAvgDepth = float.Parse( txtAvgDepth.Text );
+			var PoolVolume = CalculateVolume( PoolLength, PoolWidth, PoolAvgDepth );
+			lblCategory.Text = CalculatePoolCategory( PoolVolume );
+			WriteTempToAvgTempBox();
+			HeatCostPerMonth( PoolVolume );
+		}
+
+
+
+
+
+		//function to calculate the surface area of the pool
+		float CalculateSurfaceArea( float length, float width )
+		{
+			var tsa = length * width;
+			txtSurfaceArea.Text = tsa.ToString("0.00");
+			return tsa;
+		}
+
+
+
+		//function to calculate the volume of the pool 
+		float CalculateVolume( float length, float width, float avgdepth )
+		{
+			var PoolVolume = length * width * avgdepth * 1000;
+			txtVolume.Text = PoolVolume.ToString();
+			return PoolVolume;
+		}
+
+
+
+
+		//function to calculate pool category based on size  
+		string CalculatePoolCategory( float volume )
+		{
+			if( volume < 500000 )
+			{
+				return "Pool category: Small";
+			}
+			else if( volume < 1500000 )
+			{
+				return "Pool category: Medium";
+			}
+			else
+			{
+				return "Pool category: Large";
+			}
+		}
+
+
+
+		// function to write temperature to average temperature box
+		void WriteTempToAvgTempBox()
+		{
+			txtTableAvgTemp.Text = "";
+			for( float temperature = 5; temperature <= 23; temperature += 1.5f )
+			{
+				txtTableAvgTemp.Text += temperature.ToString() + "\r\n";
+			}
+		}
+
+
+
+		// function to calculate and write heating cost per month to $ per month box
+		void HeatCostPerMonth( float volume )
+		{
+			txtTableDollars.Text = "";
+			for( float temperature = 5; temperature < 23; temperature += 1.5f )
+			{
+				txtTableDollars.Text += (( 25 - temperature ) * volume / 32500).ToString("0") + "\r\n";
+			}
+
+        }
+
+
+
+
+		//the following section has the boolean tests for the numbers input for length/width/avgdepth input fields
+
+		bool ValidLength( float LengthValueTest )	 //testing the validity of the length that has been entered
+		{
+			string Message;
+			if( LengthValueTest > txtLengthLower && LengthValueTest < txtLengthUpper )
+			{
+				return true;
+			}
+			else
+			{
+				Message = "The length number entered is invalid.";
+				MessageBox.Show( Message, "Invalid Number", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+				return false;
+			}
+
+		}
+
+		bool ValidWidth( float WidthValueTest )	// testing the validity of the width that has been entered
+		{
+			string Message;
+			if( WidthValueTest > txtWidthLower && WidthValueTest < txtWidthUpper )
+			{
+				return true;
+			}
+			else
+			{
+				Message = "The width number entered is invalid.";
+				MessageBox.Show( Message, "Invalid Number", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+				return false;
+			}
+		}
+
+		bool ValidAvgDepth( float AvgDepthValueTest )	 // testing the validity of the depth that has been entered
+		{
+			string Message;
+			if( AvgDepthValueTest > txtAvgDepthLower && AvgDepthValueTest < txtAvgDepthUpper )
+			{
+				return true;
+			}
+			else
+			{
+				Message = "The average depth number entered is invalid.";
+				MessageBox.Show( Message, "Invalid Number", MessageBoxButtons.OK, MessageBoxIcon.Exclamation );
+				return false;
+			}
+		}
+	}
+}
